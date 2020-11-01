bea96419f00b9e05605f101a2cc301c4247b9d36
Samuel Hammersberg <samuel.hammersberg@gmail.com>
2020-11-01 22:44:47 +0100

    Added ThreeDim (Thanks AnzoDK!).

--- DimArray.cs
+++ DimArray.cs
@@ -9,10 +9,9 @@ namespace DimArray
 
         public TwoDim(int x, int y)
         {
-            var newData = new T[x * y];
             X = x;
             Y = y;
-            _data = newData;
+            _data = new T[x * y];
         }
 
         public T this[int xIndex, int yIndex]
@@ -56,6 +55,59 @@ namespace DimArray
             s += "]";
 
             return s;
+        }
+    }
+
+    public class ThreeDim<T>
+    {
+        private T[] _data;
+
+        public int X;
+        public int Y;
+        public int Z;
+
+        public ThreeDim(int x, int y, int z)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+            _data = new T[x * y * z];
+        }
+
+        public T this[int xIndex, int yIndex, int zIndex]
+        {
+            get
+            {
+                return _data[xIndex + X * (yIndex + Y * zIndex)];
+            }
+
+            set
+            {
+                _data[xIndex + X * (yIndex + Y * zIndex)] = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            string s = "[";
+
+            for (int x = 0; x < X; x++)
+            {
+                for (int y = 0; y < Y; y++)
+                {
+                    string temp = "[";
+                    for (int z = 0; z < Z; z++)
+                    {
+                        temp += this[x, y, z];
+                        if (z != this.Z - 1) temp += ", ";
+                    }
+                    s += temp + "]";
+
+                    if (x != X - 1 || y != Y - 1) s += ",\t";
+                }
+                if (x != X - 1) s += "\n";
+            }
+            return s + "]";
         }
     }
 }
